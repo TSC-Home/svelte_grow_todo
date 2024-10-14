9cf9089de9855977f9df9904a5ec33bb428333fe
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-10-14 22:38:20 +0200

    Refactor todos component: Add tasks prop and iterate over tasks in the list

--- src/lib/components/todos.svelte
+++ src/lib/components/todos.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	export let tasks;
+	console.log(tasks);
 </script>
 
 <div class="h-fit rounded-lg bg-white p-6 shadow-md">
@@ -29,27 +31,35 @@
 		<div class="mb-4">
 			<h3 class="mb-2 text-lg font-semibold text-gray-700">13.10.2024</h3>
 			<ul class="space-y-2">
-				<li class={`flex items-center justify-between rounded-md bg-green-50 p-3`}>
-					<div class="flex items-center gap-x-2">
-						<input type="checkbox" class="form-checkbox h-5 w-5 text-green-600" />
-						<span class={true ? 'text-green-500 line-through' : 'text-green-800'}>
-							Mathe lernen
-						</span>
-					</div>
-					<div class="flex w-fit items-center gap-x-2">
-						<span class="text-sm text-green-600">00:44:13</span>
-						<button class="mr-2 rounded bg-blue-500 px-2 py-1 text-xs text-white hover:bg-blue-600">
-							{true ? 'Stop' : 'Start'}
-						</button>
-						<button
-							class="icon {true ? 'text-orange-600/60' : 'text-green-600/60'}"
-							title="Keep task"
-						>
-							{true ? 'keep_off' : 'keep'}
-						</button>
-						<button class="icon text-red-600/80" title="Delete"> delete </button>
-					</div>
-				</li>
+				{#each tasks as task}
+					<li class={`flex items-center justify-between rounded-md bg-green-50 p-3`}>
+						<div class="flex items-center gap-x-2">
+							<input
+								type="checkbox"
+								value={tasks.checked}
+								class="form-checkbox h-5 w-5 text-green-600"
+							/>
+							<span class={true ? 'text-green-500 line-through' : 'text-green-800'}>
+								{task.text}
+							</span>
+						</div>
+						<div class="flex w-fit items-center gap-x-2">
+							<span class="text-sm text-green-600">00:44:13</span>
+							<button
+								class="mr-2 rounded bg-blue-500 px-2 py-1 text-xs text-white hover:bg-blue-600"
+							>
+								{true ? 'Stop' : 'Start'}
+							</button>
+							<button
+								class="icon {true ? 'text-orange-600/60' : 'text-green-600/60'}"
+								title="Keep task"
+							>
+								{true ? 'keep_off' : 'keep'}
+							</button>
+							<button class="icon text-red-600/80" title="Delete"> delete </button>
+						</div>
+					</li>
+				{/each}
 			</ul>
 		</div>
 	</div>
--- src/routes/home/[userid]/+page.server.ts
+++ src/routes/home/[userid]/+page.server.ts
@@ -1,5 +1,9 @@
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+export const load = async ({ locals }) => {
+	try {
+		const tasks = await locals.pb.collection('tasks').getFullList();
+		return { tasks };
+	} catch (error) {
+		console.log(error);
+		return { tasks: [] };
+	}
+};
